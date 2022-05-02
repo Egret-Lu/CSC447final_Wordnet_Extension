41c715e9f650fa433c314580a96ab0fef17c8d11
Egret-Lu <46036791+Egret-Lu@users.noreply.github.com>
2022-05-02 18:30:13 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 
 from nltk.corpus import wordnet as wn
 import sentiwordnet
+import verbnet
+    
+
 
 if __name__ == "__main__":
     good = wn.synset('good.a.13')
     print(f'sentiment score of {good.name()}: {good.senti()}')
     print(f'micro sentiment score of {good.name()}: {good.micro_senti()}')
+    encourage = wn.synset('encourage.v.02')
+    #get verbnet class list for encourage.v.02
+    print(encourage.vn_classes())
+    #get frame list for the first class of encourage.v.02 which is accept-77
+    print(encourage.vn_classes()[0].get_frames())
+    print(encourage.vn_classes()[0].get_roles())
 
     
